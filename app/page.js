ab207bf59ff78ed45f326ad28ce4ee75350a16b2
Idil Dogan <idilaydindogan@gmail.com>
2023-03-18 14:43:45 +0300

    Fetch data from tmdb api in home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import data from "../data.json";
+import Results from "@/components/Results";
 
 const inter = Inter({ subsets: ["latin"] });
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+	const genre = searchParams.genre || "fetchTrending";
+
+	const response = await fetch(
+		`https://api.themoviedb.org/3/${
+			genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+		}?api_key=${API_KEY}&language=en-US&page=1`,
+		{ next: { revalidate: 10000 } }
+	);
+
+	if (!response.ok) {
+		throw new Error("Fetching Error");
+	}
+
+	const data = await response.json();
+	const results = data.results;
+
 	return (
-		<main className="flex flex-col items-center justify-center w-full px-20 mx-auto mt-16"></main>
+		<main className="flex flex-col items-center justify-center w-full px-20 mx-auto mt-16">
+			<Results results={results} />
+		</main>
 	);
 }
