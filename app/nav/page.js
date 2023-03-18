5aeca22fbcf979f5eb28516c89a57ee6a670367d
Idil Dogan <idilaydindogan@gmail.com>
2023-03-18 17:07:38 +0300

    Upcoming added to nav

--- app/nav/page.js
+++ app/nav/page.js
@@ -10,10 +10,21 @@ async function Nav({ searchParams }) {
 
 	const response = await fetch(
 		`https://api.themoviedb.org/3/${
-			genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+			genre === "popular"
+				? "movie/popular"
+				: genre === "fetchTopRated"
+				? "movie/top_rated"
+				: "trending/all/week"
 		}?api_key=${API_KEY}&language=en-US&page=1`,
 		{ next: { revalidate: 10000 } }
 	);
+
+	// const response = await fetch(
+	// 	`https://api.themoviedb.org/3/${
+	// 		genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+	// 	}?api_key=${API_KEY}&language=en-US&page=1`,
+	// 	{ next: { revalidate: 10000 } }
+	// );
 
 	if (!response.ok) {
 		throw new Error("Fetching Error");
--- components/NavBar.js
+++ components/NavBar.js
@@ -4,6 +4,7 @@ import NavbarItem from "@/components/NavbarItem";
 const NavBar = () => {
 	return (
 		<div className="flex justify-center p-4 space-x-6 font-medium lg:text-lg dark:bg-gray-600 bg-amber-500 ">
+			<NavbarItem title="Upcoming" params="popular" />
 			<NavbarItem title="Trending" params="fetchTrending" />
 			<NavbarItem title="Top Rated" params="fetchTopRated" />
 		</div>
