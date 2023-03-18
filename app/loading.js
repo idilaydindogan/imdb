4dd3a2bf0cd1f4250aeabb99c59c7f29389d0f83
Idil Dogan <idilaydindogan@gmail.com>
2023-03-18 15:29:50 +0300

    Loading page added

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+	return (
+		<div className="flex justify-center mt-16 ">
+			<img src="spinner.svg" alt="loading..." className="h-20" />
+		</div>
+	);
+};
+
+export default loading;
